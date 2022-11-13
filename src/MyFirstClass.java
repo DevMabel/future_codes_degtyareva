400f32766932fd43e3f9b2f099ecd75519602acb
DevMabel <vgyyyyy@google.com>
2022-11-13 15:48:43 +0300

    Fix

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -1,6 +1,6 @@
 public class MyFirstClass {
     public static void main(String[] args) {
-        System.out.println("I will learning to program!dfsdfsdfsdfjsdksdkdlk");
+        System.out.println("I will learning to program");
     }
 }
 
