c2ba96ab5ff73b2bd87aaac0fbfa6d68a27cd4f2
DevMabel <vgyyyyy@google.com>
2022-12-05 22:22:52 +0300

    Fix

---
+++ src/Home10.java
@@ -0,0 +1,32 @@
+public class Home10 {
+
+
+    public static void main(String[] args) {
+        System.out.println();
+        int[] sort = new int[100];
+        for (int i = 0; i != 100; i++) {
+            int b = (int) (Math.random() * 100) + 1;
+            System.out.print(b + " ");
+            sort[i] = b;
+        }
+        for (int i = 0; i < sort.length - 1; i++) {
+            int maxElementIndex = i;
+            for (int j = i + 1; j < sort.length; j++) {
+                if (sort[maxElementIndex] < sort[j]) {
+                    maxElementIndex = j;
+                }
+            }
+
+            if (maxElementIndex != i) {
+                int temp = sort[i];
+                sort[i] = sort[maxElementIndex];
+                sort[maxElementIndex] = temp;
+            }
+        }
+        System.out.println("Сортированные данные из масива:");
+        System.out.println();
+        for (int i = 0; i != 100; i++) {
+            System.out.print(sort[i] + " ");
+        }
+    }
+}
